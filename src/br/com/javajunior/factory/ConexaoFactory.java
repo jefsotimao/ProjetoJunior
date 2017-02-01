5d1f6a323fd8239ca3bc18f6ff812fa0fbaff423
jefsotimao <jefersonc2012@gmail.com>
2017-02-01 21:42:11 -0200

    Update ConexaoFactory.java

--- src/br/com/javajunior/factory/ConexaoFactory.java
+++ src/br/com/javajunior/factory/ConexaoFactory.java
@@ -1,5 +1,15 @@
 package br.com.javajunior.factory;
 
 public class ConexaoFactory {
+  
+  
+	private static final String USUARIO="root";
+	private static final String SENHA="yusuke";
+	private static final String URL="jdbc:mysql://localhost:3306/agenda";
+	
+	public static Connection conectar() throws SQLException{
+		Connection conexao = DriverManager.getConnection(URL,USUARIO,SENHA);
+		return conexao;
+	}
 
 }
